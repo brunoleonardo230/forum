2f942bd76b9b99d603ec39d95a5af1b4ac491d15
Bruno Leonardo <bruno.leonardo23@hotmail.com>
2021-08-27 22:13:36 -0300

    Model, migrates reply e Thread

---
+++ app/Thread.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    protected $fillable = ['title', 'body', 'slug'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class)->orderBy('created_at', 'DESC');
+    }
+
+//    public function channel()
+//    {
+//        return $this->belongsTo(Channel::class);
+//    }
+}
